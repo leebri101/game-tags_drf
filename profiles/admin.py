9093ca68814574f6829eb9ddb8f606425961a5d8
leebri101 <>
2023-11-26 11:37:30 +0000

    Creation of admin display

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(amdin.ModelAdmin):
+    """Profile Admin"""
+    list_display = (
+        'user',
+        'avatar',
+        'user_name',
+        'first_name',
+        'last_name',
+        'bio',
+    )
