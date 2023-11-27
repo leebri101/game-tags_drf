960a39f31a0edc6a02e8ece943549e185f682aba
leebri101 <>
2023-11-27 18:24:45 +0000

    Game model created

--- games/models.py
+++ games/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from datetime import date
 
-# Create your models here.
+
+class Game(models.Model):
+    """Model for games"""
+    name = model.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+        verbose_name='Games Name:',
+        help_text=(
+            'format: required, max_length=255'
+        )
+    )
+    game_bio = models.TextField(
+        max_length=255,
+        blank=True,
+        verbose_name='Game Bio:',
+        help_text=(
+            'format: not required, max_length=255'
+        )
+    )
+    image = CloudinaryField(
+        'image', default='placeholder'
+    )
+    genre = models.ForeignKey(
+        Genre, on_delete=models.CASCADE
+        )
+    
