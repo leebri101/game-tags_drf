96c071af7c4f6b24f93b307af44e508559c348db
leebri101 <>
2023-11-28 11:42:18 +0000

    Format for Genre model created

--- genre/models.py
+++ genre/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Genre(models.Model):
+    """Model for genres of games"""
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+        verbose_name='Genre:',
+        help_text=(
+            'format: required, max_length=255'
+        )
+    )
+    description = models.TextField(
+        max_length=255,
+        blank=True,
+        verbose_name='Description:',
+        help_text=(
+            'format: not required, max_length=255'
+        )
+    )
+    image = CloudinaryField(
+        'image', default='placeholder'
+    )
