ec585f4d72dfbe82c98b44d6fd6c8893d40eb1c8
leebri101 <>
2023-11-28 12:29:48 +0000

    Comments app created

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from posts.model import Posts
+from datetime import datetime
 
-# Create your models here.
+
+class Comments(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Posted at:',
+    )
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Updated at:',
+    )
+    description = models.TextField(
+        max_length=255,
+        blank=True,
+        verbose_name='Description:',
+        help_text=(
+            'format: not required, max_length=255'
+        )
+    )
