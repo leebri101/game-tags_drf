4fb944cf4f1c889afca4d91c8e4979ad801e6e83
leebri101 <>
2023-11-28 14:22:17 +0000

    Followers model created

--- followers/models.py
+++ followers/models.py
@@ -1,2 +1,23 @@
 from django.db import models
-from 
+from odjango.contrib.auth.models import User
+from datetime import datetime
+
+
+class Follower(models.Model):
+    """Model for Followers"""
+    owner = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+    followed = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created at:',
+    )
