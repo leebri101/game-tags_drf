ab6f4e594249a855dcca35b5dae19ccaa609dc8a
leebri101 <>
2023-11-28 11:33:56 +0000

    Removal of genre to determine correct parameters

--- profiles/models.py
+++ profiles/models.py
@@ -64,6 +64,6 @@ class Profile(models.Model):
             'format: not required, max_length=255'
         )
     )
-    favourite_games = models.ManyToManyField(
+    """favourite_games = models.ManyToManyField(
         User, related_name='favourite_games'
-    )
+    )"""
