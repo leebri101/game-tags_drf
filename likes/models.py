f637ac912283201f6d9a32ba9ddde053359e48f9
leebri101 <>
2023-11-28 12:55:08 +0000

    Creation of model structure for likes

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from posts.models import Post
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+class Like(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created at:',
+    )
+
