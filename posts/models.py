65fc7aa7f9838d2e227a99d03f49f4cdaebb53f2
leebri101 <>
2023-11-28 12:10:09 +0000

    Creation of posts model app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from datetime import date
 
-# Create your models here.
+
+class Posts(models.Model):
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Posted at:',
+    )
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Updated at:',
+    )
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        null=True,
+        verbose_name='Title:',
+        help_text=(
+            'format: not required, max_length=255'
+        )
+    )
+    description = models.TextField(
+        max_length=255,
+        blank=True,
+        verbose_name='Description of Post:',
+        help_text=(
+            'format: not required, max_length=255'
+        )
+    )
+    image = CloudinaryField(
+        'image', default='placeholder'
+    )
