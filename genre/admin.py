52d977d99cac0856cccdd6707f73d3f3b3e887d2
leebri101 <>
2023-12-04 13:06:25 +0000

    Admin.py for genre created

--- genre/admin.py
+++ genre/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Genre
 
-# Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Genre Admin Display,Filter,Search fields"""
+    list_display = (
+        'name',
+        'description',
+        'image',
+    )
+    list_filter = (
+        'name',
+        'description',
+    )
+    search_fields = (
+        'name',
+        'description'
+    )
