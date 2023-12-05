a9294ddf09f22f70aaf879a4c747d98076034683
leebri101 <>
2023-12-05 12:00:43 +0000

    Added post_save function

--- profiles/models.py
+++ profiles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from games.models import Game
@@ -71,3 +72,16 @@ class Profile(models.Model):
         null=True,
         related_name='profile'
     )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.user}'s profile"
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+post_save.connect(create_profile, sender=User)
