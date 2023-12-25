022957ba34211d2c7ab6a92ec383ed571c9d45fd
leebri101 <>
2023-12-25 11:34:46 +0000

    Change of model structure

--- profiles/models.py
+++ profiles/models.py
@@ -1,64 +1,55 @@
 from django.db import models
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from games.models import Game
-from datetime import date
 
 
 class Profile(models.Model):
-    """Models for profiles"""
+    """Model for profiles"""
+
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
         related_name='profile',
         verbose_name='User',
-        help_text=(
-            'format: required, unique=True'
-        )
+        help_text='Required, unique.'
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
-        verbose_name='Created at:',
+        verbose_name='Created at'
     )
     updated_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Updated at:',
+        auto_now=True,
+        verbose_name='Updated at'
     )
     user_name = models.CharField(
         max_length=100,
         blank=False,
         null=True,
-        verbose_name='Username:',
-        help_text=(
-            'Text input REQUIRED, (max is length 100 characters)'
-        )
+        verbose_name='Username',
+        help_text='Required, max length: 100 characters.'
     )
     first_name = models.CharField(
         max_length=150,
         blank=False,
         null=True,
-        verbose_name='First Name:',
-        help_text=(
-            'Text input REQUIRED, (max is length 150 characters)'
-        )
+        verbose_name='First Name',
+        help_text='Required, max length: 150 characters.'
     )
     last_name = models.CharField(
         max_length=150,
         blank=False,
         null=True,
-        verbose_name='Last Name:',
-        help_text=(
-            'Text input REQUIRED, (max is length 150 characters)'
-        )
+        verbose_name='Last Name',
+        help_text='Required, max length: 150 characters.'
     )
     bio = models.TextField(
         max_length=255,
         blank=True,
-        verbose_name='Bio:',
-        help_text=(
-            'Text input optional, (max is length 255 characters)'
-        )
+        verbose_name='Bio',
+        help_text='Optional, max length: 255 characters.'
     )
     avatar = CloudinaryField(
         'avatar',
@@ -69,19 +60,18 @@ class Profile(models.Model):
     favourite_games = models.ManyToManyField(
         Game,
         blank=True,
-        related_name='profile'
+        related_name='profiles'
     )
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.owner}'s profile"
+        return f"{self.user}'s profile"
 
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
+    """Signal receiver to create a profile when a new user is created."""
     if created:
-        Profile.objects.create(owner=instance)
-
-
-post_save.connect(create_profile, sender=User)
+        Profile.objects.create(user=instance)
