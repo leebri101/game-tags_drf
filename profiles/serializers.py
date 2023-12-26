6c8e3c2c34c3a6f82f4eac0ab982135ac54cd550
leebri101 <>
2023-12-26 14:28:12 +0000

    Changes to serializers to display Json format

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,10 +5,9 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
 
-
-class Meta:
-    model = Profile
-    fields = [
-        'id', 'user', 'created_at', 'updated_at', 'user_name', 'first_name',
-        'last_name', 'bio', 'avatar', 'favourite_game',
-    ]
+    class Meta:
+        model = Profile
+        fields = [
+            'id', 'user', 'created_at', 'updated_at', 'user_name', 'first_name',
+            'last_name', 'bio', 'avatar', 'favourite_games',
+            ]
